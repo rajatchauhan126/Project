c7c6ba08edc558b2e55078b96674793c66aead81
Sagar Patke <sagarpatke@gmail.com>
2017-06-17 19:49:59 +0530

    fixed merge complict in cassandra integration and dev-wave-18

--- api/mailbox/mailbox.spec.js
+++ api/mailbox/mailbox.spec.js
@@ -1,101 +1,67 @@
-/* eslint prefer-arrow-callback:0, func-names:0 */
+require('chai').should();
+
 const app = require('../../app');
 
-require('chai').should();
+const expect = require('chai').expect;
+// require('chai').should();
+const should = require('chai').should();
 
 const request = require('supertest');
 
-const mailboxDao = require('../../dao/mailbox/');
+const circleDAO = require('../../dao/mailbox');
 
-
-<<<<<<< HEAD
-describe('/mailbox api', function () {
-  let id;
-  it('should create a new mailbox', function (done) {
+describe('/mailbox api', () => {
+  // const circleId = '1629d450-5279-11e7-a845-d9c5443eaaa0';
+  let mailboxId;
+  it('it should create a new mailbox', (done) => {
     request(app)
-      .post('/mailbox')
+      .post('/mailbox/')
       .expect(201)
       .expect('Content-Type', /json/)
-      .end(function (err, res) {
+      .end((err, res) => {
         if (err) { done(err); return; }
-        res.body.should.have.property('id').equal(res.body.id).a('string');
-        id = res.body.id;
-        mailboxDao.checkIfMailboxExists(id).should.be.equal(true);
-        done();
+        expect(res.body).to.have.property('id');
+        expect(res.body.id).to.be.a('string');
+        mailboxId = res.body.id;
+        mailboxDAO.checkIfCircleExists(mailboxId, (error, mailboxExists) => {
+          if (err) { done(err); return; }
+          mailboxExists.should.be.equal(true);
+          done();
+        });
       });
   });
 
-  it('should delete a mailbox', function (done) {
-    mailboxDao.checkIfMailboxExists(id).should.be.equal(true);
+  it('should delete a mailbox', (done) => {
     request(app)
-      .delete(`/mailbox/${id}`)
+      .delete(`/mailbox/${mailboxId}`)
       .expect(200)
       .expect('Content-Type', /json/)
-      .end(function (err, res) {
+      .end((err, res) => {
         if (err) { done(err); return; }
-        res.body.should.have.property('id').equal(res.body.id).a('string');
-        mailboxDao.checkIfMailboxExists(id).should.be.equal(false);
-        done();
+        expect(res.body.id).to.equal(mailboxId);
+        mailboxDAO.checkIfMailboxExists(mailboxId, (error, circleExists) => {
+          console.log('circleExists:', typeof circleExists);
+          circleExists.should.be.equal(false);
+          // console.log('circle', circleExists);
+          done();
+        });
       });
   });
 
-  it('should return an error when we try to delete a mailbox that does not exist', function (done) {
-    mailboxDao.checkIfMailboxExists(id).should.be.equal(false);
+  it('should fail when we try to delete a circle id that does not exist', (done) => {
     request(app)
-      .delete(`/mailbox/${id}`)
+      .delete(`/circle/${circleId}`)
       .expect(404)
       .expect('Content-Type', /json/)
-      .end(function (err, res) {
-        if (err) { done(err); return; }
-        res.body.should.have.property('message').equal(`Mailbox with id ${id} does not exist`);
-        mailboxDao.checkIfMailboxExists(id).should.be.equal(false);
-        done();
+      .end((err, res) => {
+        if (err) { done(err); } else {
+          circleDAO.checkIfCircleExists(circleId, (error, circleExists) => {
+            if (error) { done(error); return; }
+            console.log('circle', JSON.stringify(circleExists));
+            circleExists.should.be.equal(false);
+            done();
+          });
+        }
       });
   });
 });
-=======
-// describe('/mailbox api', () => {
-//   let id;
-//   it('should create a new mailbox', (done) => {
-//     request(app)
-//       .post('/mailbox')
-//       .expect(201)
-//       .expect('Content-Type', /json/)
-//       .end((err, res) => {
-//         if (err) { done(err); return; }
-//         expect(res.body).to.have.property('id');
-//         expect(res.body.id).to.be.a('string');
-//         id = res.body.id;
-//         expect(mailboxDao.checkIfMailboxExists(id)).to.equal(true);
-//         done();
-//       });
-//   });
-
-//   it('should delete a mailbox', (done) => {
-//     request(app)
-//       .delete(`/mailbox/${id}`)
-//       .expect(200)
-//       .expect('Content-Type', /json/)
-//       .end((err, res) => {
-//         if (err) { done(err); return; }
-//         expect(res.body).to.have.property('id');
-//         expect(res.body.id).to.equal(id);
-//         expect(mailboxDao.checkIfMailboxExists(id)).to.equal(false);
-//         done();
-//       });
-//   });
-
-//   it('should return an error when we try to delete a mailbox that does not exist', (done) => {
-//     request(app)
-//       .delete(`/mailbox/${id}`)
-//       .expect(404)
-//       .expect('Content-Type', /json/)
-//       .end((err, res) => {
-//         if (err) { done(err); return; }
-//         expect(res.body).to.have.property('message').equal(`Mailbox with id ${id} does not exist`);
-//         expect(mailboxDao.checkIfMailboxExists(id)).to.equal(false);
-//         done();
-//       });
-//   });
-// });
->>>>>>> cassandra-integration
