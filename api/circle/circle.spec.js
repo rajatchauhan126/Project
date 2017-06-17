8a42f4009e47ec4adbfdec1ebd8948548417b9fe
Sagar Patke <sagarpatke@gmail.com>
2017-06-17 20:50:01 +0530

    made async code for circle.spec, mailbox.spec

--- api/circle/circle.spec.js
+++ api/circle/circle.spec.js
@@ -1,42 +1,25 @@
-/* eslint prefer-arrow-callback:0, func-names:0 */
-const app = require('../../app');
-
 require('chai').should();
 
-const expect = requrire('Chai').expect;
+const app = require('../../app');
+
+const expect = require('chai').expect;
+// require('chai').should();
+const should = require('chai').should();
 
 const request = require('supertest');
 
+
 const circleDAO = require('../../dao/circle');
 
-const JWT = require('jwt-async');
-
-const jwt = new JWT();
-
-const configFile = require('fs').readFileSync('secret.json');
-
-const config = JSON.parse(configFile);
-
-jwt.setSecret(config.secretKey);
-
-describe('/circle api', function () {
+describe('/circle api', () => {
+  // const circleId = '1629d450-5279-11e7-a845-d9c5443eaaa0';
   let circleId;
-  let token;
-  before(function (done) {
-    jwt.sign({ someClaim: 'data' }, function (err, data) {
-      if (err) { return err; }
-      token = data;
-      return data;
-    });
-    done();
-  });
-  it('it should create a new circle', function (done) {
+  it('it should create a new circle', (done) => {
     request(app)
       .post('/circle/')
-      .set('Authorization', `Bearer ${token}`)
       .expect(201)
       .expect('Content-Type', /json/)
-      .end(function (err, res) {
+      .end((err, res) => {
         if (err) { done(err); return; }
         expect(res.body).to.have.property('id');
         expect(res.body.id).to.be.a('string');
@@ -49,37 +32,30 @@ describe('/circle api', function () {
       });
   });
 
-  it('should delete a circle', function (done) {
-    circleDAO.checkIfCircleExists(circleId).should.be.equal(true);
+  it('should delete a circle', (done) => {
     request(app)
       .delete(`/circle/${circleId}`)
-      .set('Authorization', `Bearer ${token}`)
       .expect(200)
       .expect('Content-Type', /json/)
-      .end(function (err, res) {
+      .end((err, res) => {
         if (err) { done(err); return; }
         expect(res.body.id).to.equal(circleId);
         circleDAO.checkIfCircleExists(circleId, (error, circleExists) => {
-          console.log('circleExists:', typeof circleExists);
           circleExists.should.be.equal(false);
-          // console.log('circle', circleExists);
           done();
         });
       });
   });
 
-  it('should fail when we try to delete a circle id that does not exist', function (done) {
-    circleDAO.checkIfCircleExists(circleId).should.be.equal(false);
+  it('should fail when we try to delete a circle id that does not exist', (done) => {
     request(app)
       .delete(`/circle/${circleId}`)
-      .set('Authorization', `Bearer ${token}`)
       .expect(404)
       .expect('Content-Type', /json/)
       .end((err, res) => {
         if (err) { done(err); } else {
           circleDAO.checkIfCircleExists(circleId, (error, circleExists) => {
             if (error) { done(error); return; }
-            console.log('circle', JSON.stringify(circleExists));
             circleExists.should.be.equal(false);
             done();
           });
--- api/mailbox/mailbox.spec.js
+++ api/mailbox/mailbox.spec.js
@@ -1,21 +1,16 @@
-require('chai').should();
-
 const app = require('../../app');
 
 const expect = require('chai').expect;
-// require('chai').should();
-const should = require('chai').should();
-
 const request = require('supertest');
 
-const circleDAO = require('../../dao/mailbox');
+const mailboxDao = require('../../dao/mailbox/');
+
 
 describe('/mailbox api', () => {
-  // const circleId = '1629d450-5279-11e7-a845-d9c5443eaaa0';
   let mailboxId;
-  it('it should create a new mailbox', (done) => {
+  it('should create a new mailbox', (done) => {
     request(app)
-      .post('/mailbox/')
+      .post('/mailbox')
       .expect(201)
       .expect('Content-Type', /json/)
       .end((err, res) => {
@@ -23,13 +18,14 @@ describe('/mailbox api', () => {
         expect(res.body).to.have.property('id');
         expect(res.body.id).to.be.a('string');
         mailboxId = res.body.id;
-        mailboxDAO.checkIfCircleExists(mailboxId, (error, mailboxExists) => {
+        mailboxDao.checkIfMailboxExists(mailboxId, (error, mailboxExists) => {
           if (err) { done(err); return; }
           mailboxExists.should.be.equal(true);
           done();
         });
       });
   });
+
 
   it('should delete a mailbox', (done) => {
     request(app)
@@ -38,27 +34,25 @@ describe('/mailbox api', () => {
       .expect('Content-Type', /json/)
       .end((err, res) => {
         if (err) { done(err); return; }
-        expect(res.body.id).to.equal(mailboxId);
-        mailboxDAO.checkIfMailboxExists(mailboxId, (error, circleExists) => {
-          console.log('circleExists:', typeof circleExists);
-          circleExists.should.be.equal(false);
-          // console.log('circle', circleExists);
+        expect(res.body).to.have.property('id');
+        mailboxDao.checkIfMailboxExists(mailboxId, (error, mailboxExists) => {
+          mailboxExists.should.be.equal(false);
           done();
         });
       });
   });
 
-  it('should fail when we try to delete a circle id that does not exist', (done) => {
+
+  it('should return an error when we try to delete a mailbox that does not exist', (done) => {
     request(app)
-      .delete(`/circle/${circleId}`)
+      .delete(`/mailbox/${mailboxId}`)
       .expect(404)
       .expect('Content-Type', /json/)
       .end((err, res) => {
         if (err) { done(err); } else {
-          circleDAO.checkIfCircleExists(circleId, (error, circleExists) => {
+          mailboxDao.checkIfMailboxExists(mailboxId, (error, mailboxExists) => {
             if (error) { done(error); return; }
-            console.log('circle', JSON.stringify(circleExists));
-            circleExists.should.be.equal(false);
+            mailboxExists.should.be.equal(false);
             done();
           });
         }
